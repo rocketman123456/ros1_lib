8cb04f3f7c85b40138262f15857ffaa27da3c52e
rocketman123456 <759094438@qq.com>
2023-10-21 14:27:19 +0800

    add ocs2 lib

---
+++ build_legged.sh
@@ -0,0 +1,6 @@
+catkin_make_isolated --install --pkg \
+    ocs2_core ocs2_thirdparty ocs2_mpc ocs2_oc ocs2_legged_robot_ros \
+    ocs2_self_collision_visualization legged_gazebo legged_controllers \
+    legged_unitree_description ocs2_ddp legged_common ocs2_qp_solver \
+    ocs2_sqp hpipm_catkin blasfeo_catkin ocs2_ipm ocs2_robotic_tools \
+    ocs2_pinocchio_interface
